1f00e6acf13260a7f63d6f261d60d533bd678688
Jan Weiß <jan@geheimwerk.de>
2014-08-23 12:00:12 +0200

    Improving comments.

--- JXExtendedFileAttributes.h
+++ JXExtendedFileAttributes.h
@@ -21,24 +21,24 @@ typedef NS_ENUM(NSUInteger, JXExtendedFileAttributesValueTypes) {
 	int _fd;
 }
 
-// Returns nil on failure
+// Returns nil on failure.
 - (instancetype)initWithURL:(NSURL *)theURL;
 - (instancetype)initWithFile:(NSString *)path;
 
-// Returns an array of NSString, returns nil on failure
+// Returns an array of NSString, returns nil on failure.
 - (NSArray *)keys;
 
-// Returns nil on failure
+// Returns nil on failure.
 - (NSData *)dataForKey:(NSString *)key;
 
-// Returns YES if successful
+// Returns YES if successful.
 - (BOOL)removeDataForKey:(NSString *)key;
 
-// Returns YES if successful
+// Returns YES if successful.
 - (BOOL)removeAllData;
 
-// Returns YES if successful
-// Removes data if value is nil
+// Returns YES if successful.
+// Removes data if value is nil.
 - (BOOL)setData:(NSData *)value forKey:(NSString *)key;
 
 // Convenience methods auto-detecting necessary conversions.
@@ -53,7 +53,7 @@ typedef NS_ENUM(NSUInteger, JXExtendedFileAttributesValueTypes) {
 - (NSString *)stringForKey:(NSString *)key;
 - (BOOL)setString:(NSString *)value forKey:(NSString *)key;
 
-// Close file immediately rather than waiting for dealloc
+// Close file immediately rather than waiting for -dealloc.
 - (void)closeFile;
 
 @end
